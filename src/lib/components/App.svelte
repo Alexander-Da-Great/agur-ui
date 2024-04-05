45dcef512869f2365bf4edfec7958c0624e3da0a
Alexander Greer <alexandergreer@AlexandersMBP2.lan>
2024-04-05 12:41:57 -0700

    adding dark mode switching

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
+  import { Button } from "$lib/components/ui/button";
+  import { toggleMode } from "mode-watcher";
 </script>
+
+<Button on:click={toggleMode} variant="outline" size="icon">Click Me</Button>
 
 <Canvas>
   <Scene />
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import mkcert from 'vite-plugin-mkcert';
+import path from "path";
+
+// export default defineConfig({
+// 	plugins: [mkcert(), sveltekit()],
+// 	ssr: {
+// 		noExternal: ['three']
+// 	},
+// 	test: {
+// 		include: ['src/**/*.{test,spec}.{js,ts}']
+// 	}
+// });
 
 export default defineConfig({
-	plugins: [mkcert(), sveltekit()],
-	ssr: {
-		noExternal: ['three']
-	},
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    server: {
+        proxy: {}
+    },
+	resolve: {
+		alias: {
+		  $lib: path.resolve("./src/lib"),
+		},
+	  },
+    plugins: [mkcert(), sveltekit()]
 });
-
