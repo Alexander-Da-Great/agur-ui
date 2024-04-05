e5d61a4cba6b7fdfc9705810f7e38d9c7656edc7
Alexander Greer <alexandergreer@AlexandersMBP2.lan>
2024-04-05 16:14:18 -0700

    attempt number one at a control panel

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,10 +1,71 @@
-<script>
+<script lang="ts">
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
   import Control from './ui/control/control.svelte';
+	import Search from "lucide-svelte/icons/search";
+	import { primaryRoutes, secondaryRoutes } from "./config.js";
+	import { cn } from "$lib/utils.js";
+	import * as Resizable from "./ui/resizable/index.js";
+	import { Separator } from "./ui/select/index.js";
+  import Nav from "./nav.svelte";
+
+	export let defaultLayout = [265, 440, 655];
+	export let defaultCollapsed = false;
+	export let navCollapsedSize: number;
+
+	let isCollapsed = defaultCollapsed;
+
+	function onLayoutChange(sizes: number[]) {
+		document.cookie = `PaneForge:layout=${JSON.stringify(sizes)}`;
+	}
+
+	function onCollapse() {
+		isCollapsed = true;
+		document.cookie = `PaneForge:collapsed=${true}`;
+	}
+
+	function onExpand() {
+		isCollapsed = false;
+		document.cookie = `PaneForge:collapsed=${false}`;
+	}
 </script>
 
-<Control></Control>
-<Canvas>
-  <Scene />
-</Canvas>
+<div class="md:hidden">
+</div>
+<div class="hidden md:block">
+	<Resizable.PaneGroup
+		direction="horizontal"
+		{onLayoutChange}
+		class="h-full max-h-[800px] items-stretch"
+	>
+		<Resizable.Pane
+			defaultSize={defaultLayout[0]}
+			collapsedSize={navCollapsedSize}
+			collapsible
+			minSize={15}
+			maxSize={20}
+			{onCollapse}
+			{onExpand}
+		>
+			<div
+				class={cn(
+					"flex h-[52px] items-center justify-center",
+					isCollapsed ? "h-[52px]" : "px-2"
+				)}
+			>
+			</div>
+      <Control></Control>
+			<Separator />
+			<Nav {isCollapsed} routes={primaryRoutes} />
+			<Separator />
+			<Nav {isCollapsed} routes={secondaryRoutes} />
+		</Resizable.Pane>
+		<Resizable.Handle withHandle />
+		<Resizable.Handle withHandle />
+		<Resizable.Pane defaultSize={defaultLayout[1]}>
+      <Canvas>
+        <Scene />
+      </Canvas>
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
+</div>
